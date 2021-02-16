f19e55e1dcb03e353bc1ce84dff02ec9deef0f82
ASUS <ASUS@LAPTOP-14A15HTO>
2021-02-16 13:08:34 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,58 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _iCarDal;
+
+        public CarManager(ICarDal iCarDal)
+        {
+            _iCarDal = iCarDal;
+        }
+
+        public IResult Add(Car car)
+        {
+            if(car.DailyPrice>0 && car.CarName.Length > 2)
+            {
+                _iCarDal.Add(car);
+                return new SuccessResult(Messages.CarAdded);
+            }
+            else
+            {
+              
+           return new ErrorResult(Messages.CarCouldNotAdded);
+            }
+           
+        }
+
+        public IDataResult<List<Car>> GetAll()
+        {
+            return new SuccessDataResult<List<Car>>(_iCarDal.GetAll(),Messages.CarsDisplay);
+        }
+
+        public IDataResult<List<CarDetailDto>> GetCarDetails()
+        {
+            return new SuccessDataResult<List<CarDetailDto>>(_iCarDal.GetCarDetails(),Messages.CarsDisplay);
+        }
+
+        public IDataResult<List<Car>> GetCarsByBrandId(int id)
+        {
+            return new SuccessDataResult<List<Car>>(_iCarDal.GetAll(p=>p.BrandId==id),Messages.CarsDisplay);
+        }
+
+        public IDataResult<List<Car>> GetCarsByColorId(int id)
+        {
+            return new SuccessDataResult<List<Car>>(_iCarDal.GetAll(p => p.ColorId == id),Messages.CarsDisplay);
+        }
+    }
+}
